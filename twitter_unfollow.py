1efb977aadcc1229c20d8571a0e4103d5ca469a1
KenMwaura1 <kemwaura@gmail.com>
2020-08-14 03:55:49 +0300

    fixed the debug error

--- twitter_unfollow.py
+++ twitter_unfollow.py
@@ -4,9 +4,8 @@ import pandas as pd
 from datetime import datetime, timedelta
 from environs import Env
 
-env = Env(
-    DEBUG=(bool, False)
-)
+env = Env()
+
 # Reading the .env file
 env.read_env()
 
